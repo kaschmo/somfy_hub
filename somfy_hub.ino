9c6c2c0f92052b46e97d45c64a5912490c0e3492
kaschmo <schmolders@gmail.com>
2017-09-01 23:06:19 +0200

    added mqtt support

--- somfy_hub.ino
+++ somfy_hub.ino
@@ -12,15 +12,16 @@
     - send 'p' to the serial terminal
   To make a group command, just repeat the last two steps with another blind (one by one)
   
-  Then:
-    - m, u or h will make it to go up
-    - s make it stop
-    - b, or d will make it to go down
-    - you can also send a HEX number directly for any weird command you (0x9 for the sun and wind detector for instance)
-*/
+  See MQTT section for commands to issue ccontrol commands to somfy
+  */
 
 #include <EEPROM.h>
-#define PORT_TX 5 //5 of PORTD = DigitalPin 5
+//required for MQTT
+#include "wifi_credentials.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#define PORT_TX 5 //5 of PORTD = DigitalPin 5. GPIO 5 equals D1
 
 #define SYMBOL 640
 #define HAUT 0x2
@@ -36,54 +37,131 @@ unsigned int rollingCode = 0;
 byte frame[7];
 byte checksum;
 
+//MQTT
+WiFiClient espClient;
+PubSubClient client(espClient);
+const char* inTopic = "cmnd/somfy_hub/#";
+const char* outTopic = "stat/somfy_hub/";
+
 void BuildFrame(byte *frame, byte button);
 void SendCommand(byte *frame, byte sync);
 
+void setup_wifi() {
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.persistent(false);
+    WiFi.mode(WIFI_OFF);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+      
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+//callback function for MQTT client
+void callback(char* topic, byte* payload, unsigned int length) {
+    payload[length]='\0'; // Null terminator used to terminate the char array
+    String message = (char*)payload;
+  
+    Serial.print("Message arrived on topic: [");
+    Serial.print(topic);
+    Serial.print("]: ");
+    Serial.println(message);
+    
+    //get last part of topic 
+    char* cmnd = "test";
+    char* cmnd_tmp=strtok(topic, "/");
+  
+    while(cmnd_tmp !=NULL) {
+      cmnd=cmnd_tmp; //take over last not NULL string
+      cmnd_tmp=strtok(NULL, "/"); //passing Null continues on string
+      //Serial.println(cmnd_tmp);    
+    }
+  
+    if (!strcmp(cmnd, "control")) {
+        if (message == "UP") {
+            Serial.println("Received Somfy Up"); 
+            BuildFrame(frame, HAUT);
+        }
+        else if (message == "DOWN") {
+            Serial.println("Received Somfy Down"); 
+            BuildFrame(frame, BAS);
+        }
+        else if (message == "STOP") {
+            Serial.println("Received Somfy Stop"); 
+            BuildFrame(frame, STOP);
+        }
+        else if (message == "PROG") {
+            Serial.println("Received Somfy Prog"); 
+            BuildFrame(frame, PROG);
+        }
+        //TODO add custom series as send over MQTT
+        SendCommand(frame, 2);
+        for(int i = 0; i<2; i++) {
+          SendCommand(frame, 7);
+        }
+    }
+    else if (!strcmp(cmnd, "reset")) {
+        Serial.print("Received reset command");
+        ESP.reset();
+    }
+}
+
+void reconnect() {
+    // Loop until we're reconnected
+    while (!client.connected()) {
+      Serial.print("Attempting MQTT connection...");
+      // Attempt to connect
+      if (client.connect("SOMFY_HUB")) {
+        Serial.println("connected");        
+        client.publish(outTopic, "Somfy Hub booted");        
+        // ... and resubscribe
+        client.subscribe(inTopic);  
+      } else {
+        Serial.print("failed, rc=");
+        Serial.print(client.state());
+        Serial.println(" try again in 5 seconds");      
+        delay(5000);
+      }
+    }
+}
 
 void setup() {
   Serial.begin(115200);
-  DDRD |= 1<<PORT_TX; // Pin 5 an output
-  PORTD &= !(1<<PORT_TX); // Pin 5 LOW
+  Serial.println("Somfy Hub booted");
+  pinMode(PORT_TX,OUTPUT);
+  //DDRD |= 1<<PORT_TX; // Pin 5 an output
+  //PORTD &= !(1<<PORT_TX); // Pin 5 LOW
+  GPOS &= !(1<<PORT_TX);
 
   if (EEPROM.get(EEPROM_ADDRESS, rollingCode) < newRollingCode) {
     EEPROM.put(EEPROM_ADDRESS, newRollingCode);
   }
   Serial.print("Simulated remote number : "); Serial.println(REMOTE, HEX);
   Serial.print("Current rolling code    : "); Serial.println(rollingCode);
+
+  //WIFI and MQTT
+  setup_wifi();                   // Connect to wifi 
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char serie = (char)Serial.read();
-    Serial.println("");
-//    Serial.print("Remote : "); Serial.println(REMOTE, HEX);
-    if(serie == 'm'||serie == 'u'||serie == 'h') {
-      Serial.println("Monte"); // Somfy is a French company, after all.
-      BuildFrame(frame, HAUT);
-    }
-    else if(serie == 's') {
-      Serial.println("Stop");
-      BuildFrame(frame, STOP);
-    }
-    else if(serie == 'b'||serie == 'd') {
-      Serial.println("Descend");
-      BuildFrame(frame, BAS);
-    }
-    else if(serie == 'p') {
-      Serial.println("Prog");
-      BuildFrame(frame, PROG);
-    }
-    else {
-      Serial.println("Custom code");
-      BuildFrame(frame, serie);
-    }
-
-    Serial.println("");
-    SendCommand(frame, 2);
-    for(int i = 0; i<2; i++) {
-      SendCommand(frame, 7);
-    }
-  }
+    if (!client.connected()) {
+        reconnect();
+      }
+      client.loop();
+  
 }
 
 
@@ -149,43 +227,54 @@ void BuildFrame(byte *frame, byte button) {
 void SendCommand(byte *frame, byte sync) {
   if(sync == 2) { // Only with the first frame.
   //Wake-up pulse & Silence
-    PORTD |= 1<<PORT_TX;
+    //PORTD |= 1<<PORT_TX;
+    GPOS |= 1<<PORT_TX;
     delayMicroseconds(9415);
-    PORTD &= !(1<<PORT_TX);
+    //PORTD &= !(1<<PORT_TX);
+    GPOS &= !(1<<PORT_TX);
     delayMicroseconds(89565);
   }
 
 // Hardware sync: two sync for the first frame, seven for the following ones.
   for (int i = 0; i < sync; i++) {
-    PORTD |= 1<<PORT_TX;
+    //PORTD |= 1<<PORT_TX;
+    GPOS |= 1<<PORT_TX;
     delayMicroseconds(4*SYMBOL);
-    PORTD &= !(1<<PORT_TX);
+    //PORTD &= !(1<<PORT_TX);
+    GPOS &= !(1<<PORT_TX);
     delayMicroseconds(4*SYMBOL);
   }
 
 // Software sync
-  PORTD |= 1<<PORT_TX;
+  //PORTD |= 1<<PORT_TX;
+  GPOS |= 1<<PORT_TX;
   delayMicroseconds(4550);
-  PORTD &= !(1<<PORT_TX);
+  //PORTD &= !(1<<PORT_TX);
+  GPOS &= !(1<<PORT_TX);
   delayMicroseconds(SYMBOL);
   
   
 //Data: bits are sent one by one, starting with the MSB.
   for(byte i = 0; i < 56; i++) {
     if(((frame[i/8] >> (7 - (i%8))) & 1) == 1) {
-      PORTD &= !(1<<PORT_TX);
+      //PORTD &= !(1<<PORT_TX);
+      GPOS &= !(1<<PORT_TX);
       delayMicroseconds(SYMBOL);
-      PORTD ^= 1<<5;
+      //PORTD ^= 1<<5;
+      GPOS ^= 1<<5;
       delayMicroseconds(SYMBOL);
     }
     else {
-      PORTD |= (1<<PORT_TX);
+      //PORTD |= (1<<PORT_TX);
+      GPOS |= (1<<PORT_TX);
       delayMicroseconds(SYMBOL);
-      PORTD ^= 1<<5;
+      //PORTD ^= 1<<5;
+      GPOS ^= 1<<5;
       delayMicroseconds(SYMBOL);
     }
   }
   
-  PORTD &= !(1<<PORT_TX);
+  //PORTD &= !(1<<PORT_TX);
+  GPOS &= !(1<<PORT_TX);
   delayMicroseconds(30415); // Inter-frame silence
 }
